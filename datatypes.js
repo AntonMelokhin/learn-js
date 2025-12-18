292d43cc29c9c42ae3a108a821fbac0d1fe662bb
AntonMelokhin <melantoxa@gmail.com>
2025-12-19 00:48:43 +0200

    Delete datatypes.js, day1.js, variable.js

--- datatypes.js
+++
@@ -1,6 +0,0 @@
-import * as data from './variable.js';
-
-console.log(typeof data.str);
-console.log(typeof data.admin);
-console.log(typeof data.dateOfBirth);
-console.log(typeof data.address);
--- variable.js
+++
@@ -1,6 +0,0 @@
-export let str = "My first string";
-export let admin = true;
-export let dateOfBirth;
-export const address = null;
-
-console.log(str,admin,dateOfBirth,address);
