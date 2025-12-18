1352267aa58dfdc855d518c5ad7b3e71a30e87aa
Anton <melantoxa@gmail.com>
2025-12-18 13:38:41 +0200

    Day 1

---
+++ day-01-basics/day1.js
@@ -0,0 +1,32 @@
+// comments can make code readable
+// welcome to 30DaysOfJavaScript
+/* comments can make code readable,
+easy to use and informative*/
+
+// go to variable.js --->
+
+// four variables without assigning values
+let number, color1, color2, color3; 
+console.log(number,color1,color2,color3);
+
+// four variables with assigned values
+const a=4, b=8, c=174, d=0;
+console.log((a+b)/(c-d));
+
+// variables to store my first name, last name, marital status, country and age in multiple lines
+let myName, mySurname, myMaritalStatus, myCountry, myAge;
+myName = "Anton";
+mySurname = "Melokhin";
+myMaritalStatus = "Married";
+myCountry = "Ukraine";
+myAge = "23";
+
+// variables to store my first name, last name, marital status, country and age in a single line
+let myName1 = "Anton", mySurname1 = "Melokhin", myMaritalStatus1 = "Married", myCountry1 = "Ukraine", myAge1 = "23";
+console.log(myName,mySurname,myMaritalStatus,myCountry,myAge);
+console.log(myName1,mySurname1,myMaritalStatus1,myCountry1,myAge1);
+
+// variables from exercise 10
+let yourAge = 24;
+console.log(`I am ${myAge} years old`);
+console.log(`You are ${yourAge} years old`);
